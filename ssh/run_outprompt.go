8da6f0cb5dd2bd8e15972684c8e1c2bed5c25656
uesugi <uesugi@cyberdefense.jp>
2019-05-31 15:56:43 +0900

    change

--- ssh/run_outprompt.go
+++ ssh/run_outprompt.go
@@ -1,18 +1,23 @@
 package ssh
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 
+	"github.com/blacknon/lssh/common"
 	"github.com/blacknon/lssh/conf"
 )
 
 // Output Prompt
-type OPrompt struct {
+type Output struct {
 	Templete   string
+	prompt     string
+	server     string
 	Count      int
 	ServerList []string
-	Conf       []conf.ServerConfig
+	Conf       map[string]conf.ServerConfig
+	AutoColor  bool
 }
 
 // @TODO:
@@ -24,20 +29,44 @@ type OPrompt struct {
 //     - $h ... Addr
 //     - $u ... User
 //     - $p ... Port
-func (op *OPrompt) Create(server string) (p string) {
+func (o *Output) Create(server string) {
+	o.server = server
+	data := o.Conf[server]
+
+	// get max length at server name
+	length := common.GetMaxLength(o.ServerList)
+
+	// set templete
+	p := o.Templete
+
 	// server info
-	p = strings.Replace(p, "$s", server, -1)
-	// p = strings.Replace(p, "$h", data.Addr, -1)
-	// p = strings.Replace(p, "$u", data.User, -1)
-	// p = strings.Replace(p, "$p", data.Port, -1)
+	p = strings.Replace(p, "$s", fmt.Sprintf("%-*s", length, server), -1)
+	p = strings.Replace(p, "$h", data.Addr, -1)
+	p = strings.Replace(p, "$u", data.User, -1)
+	p = strings.Replace(p, "$p", data.Port, -1)
+
+	o.prompt = p
+}
+
+// update variable value
+func (o *Output) GetPrompt() (up string) {
+	// replace variable value
+	up = strings.Replace(o.prompt, "$n", strconv.Itoa(0), -1)
 
 	return
 }
 
-// update variable value
-func (op *OPrompt) Update(p string) (up string) {
-	// replace variable value
-	up = strings.Replace(p, "$n", strconv.Itoa(0), -1)
+func printOutput(o *Output, output chan []byte) {
+	// print output
+	for data := range output {
+		str := strings.TrimRight(string(data), "\n")
 
-	return
+		if len(o.ServerList) > 1 {
+			oPrompt := o.GetPrompt()
+			fmt.Printf("%s %s\n", oPrompt, str)
+		} else {
+			fmt.Printf("%s\n", str)
+		}
+	}
+
 }
