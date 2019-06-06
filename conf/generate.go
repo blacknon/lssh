6ee6311d448d8c6e0ddeca6645fac573ba172b77
uesugi <uesugi@cyberdefense.jp>
2019-06-06 18:34:57 +0900

    add comment

--- conf/generate.go
+++ conf/generate.go
@@ -11,6 +11,10 @@ import (
 	"github.com/kevinburke/ssh_config"
 )
 
+// @TODO: 項目のアップデートをする(v0.5.6)
+// @TODO: import処理をできるように考慮する(v0.5.6)
+// @TODO: .ssh/configの読み込み、Parse処理を分離するので、そちらを参照させる(v0.5.6)
+
 func ReadSshConfig() (cfg *ssh_config.Config, err error) {
 	// Open ~/.ssh/config
 	sshConfigFile := common.GetFullPath("~/.ssh/config")
