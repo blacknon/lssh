20888da664fbec809c1e3ec295be889948c56773
uesugi <uesugi@cyberdefense.jp>
2019-07-10 13:09:10 +0900

    add

--- ssh/doc.go
+++ ssh/doc.go
@@ -1,4 +1,17 @@
 /*
 ssh package is that describes the whole process of connecting to ssh with lssh.
+
+TODO(blacknon): 以下の機能について、汎用ライブラリとして外出ししてしまう
+    - 認証系(AuthMap系？)
+        - 鍵認証
+        - パスワード認証
+        - 証明書認証
+        - PKCS11認証
+        - ssh-agent認証
+    - プロキシ系
+        - http/httpsプロキシ
+        - socks5プロキシ
+        - ssh多段プロキシ
+    - ターミナル接続周り(キー入力をちゃんといけるように)
 */
 package ssh
