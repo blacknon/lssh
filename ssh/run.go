8d816be860557119e534d00e2d0bef65bcce2b1a
uesugi <uesugi@cyberdefense.jp>
2019-07-17 16:08:17 +0900

    before test

--- ssh/run.go
+++ ssh/run.go
@@ -2,7 +2,6 @@ package ssh
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"runtime"
@@ -116,102 +115,4 @@ func (r *Run) cmd() {
 		connect.ForwardX11 = true
 	}
 
-}
-
-func (r *Run) shell() {
-	// server config
-	server := r.ServerList[0]
-	config := r.Conf.Server[server]
-
-	// Craete sshlib.Connect (Connect Proxy loop)
-	connect, err := r.createSshConnect(server)
-
-	// x11 forwarding
-	if config.X11 {
-		connect.ForwardX11 = true
-	}
-
-	// ssh-agent
-	connect.ConnectSshAgent()
-	if config.SSHAgentUse {
-		connect.Agent = r.Agent
-		connect.ForwardSshAgent(session)
-	}
-
-	// Port Forwarding
-	if r.PortForwardLocal != "" && r.PortForwardRemote != "" {
-		err := connect.TCPForward(r.PortForwardLocal, r.PortForwardRemote)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-
-	// Connect shell
-	connect.Shell()
-}
-
-//
-func createSshConnect(server string, config conf.Config) (connect *sshlib.Connect, err error) {
-	serverConfig := conf.ServerConfig[server]
-
-}
-
-// getProxyList return proxy list and map by proxy type.
-func getProxyList(server string, config conf.Config) (proxyList []string, proxyType map[string]string, err error) {
-	var targetType string
-	var preProxy, preProxyType string
-
-	targetServer := server
-	proxyType = map[string]string{}
-
-	for {
-		isOk := false
-
-		switch targetType {
-		case "http", "https", "socks5":
-			preProxyConf, isOk := config.Proxy[targetServer]
-			preProxy = preProxyConf.Proxy
-			preProxyType = preProxyConf.ProxyType
-
-		default:
-			var preProxyConf conf.ServerConfig
-			preProxyConf, isOk = config.Server[targetServer]
-			preProxy = preProxyConf.Proxy
-			preProxyType = preProxyConf.ProxyType
-		}
-
-		// not use pre proxy
-		if preProxy == "" {
-			break
-		}
-
-		if !isOk {
-			err = fmt.Errorf("Not Found proxy : %s", targetServer)
-			return nil, nil, err
-		}
-
-		// set proxy info
-		proxy := new(Proxy)
-		proxy.Name = preProxy
-
-		switch preProxyType {
-		case "http", "https", "socks5":
-			proxy.Type = preProxyType
-		default:
-			proxy.Type = "ssh"
-		}
-
-		proxyList = append(proxyList, proxy.Name)
-		proxyType[proxy.Name] = proxy.Type
-
-		targetServer = proxy.Name
-		targetType = proxy.Type
-	}
-
-	// reverse proxyServers slice
-	for i, j := 0, len(proxyList)-1; i < j; i, j = i+1, j-1 {
-		proxyList[i], proxyList[j] = proxyList[j], proxyList[i]
-	}
-
-	return
 }
