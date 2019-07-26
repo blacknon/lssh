a30f648437d4faa53e04ed02a29c0abe4d75d5ad
Blacknon <blacknon@orebibou.com>
2019-07-26 09:12:00 +0900

    add comment

--- ssh/pshell_parse.go
+++ ssh/pshell_parse.go
@@ -1,1 +1,35 @@
 package ssh
+
+type pipeLine struct {
+	Args    []string
+	Oprator string
+}
+
+// //
+// func (ps *pShell) parseCmdPipeLine() (pLine []string) {
+
+// }
+
+//
+// func parseCallExpr(cmd *syntax.CallExpr) (pLine []string) {
+// 	printer := syntax.NewPrinter()
+
+// 	for _, arg := range cmd.Args {
+// 		for _, part := range arg.Parts {
+// 			buf := new(bytes.Buffer)
+// 			printer.Print(buf, part)
+// 			pLine = append(pLine, buf.String())
+
+// 			switch p := part.(type) {
+// 			case *syntax.ProcSubst:
+// 				fmt.Printf("%s: %s\n", buf.String(), "*syntax.ProcSubst")
+// 				fmt.Println(p.Op.String())
+// 				fmt.Println(p.Stmts)
+// 			case *syntax.CmdSubst:
+// 				fmt.Printf("%s: %s\n", buf.String(), "*syntax.CmdSubst")
+// 				fmt.Println(p.Stmts)
+// 			}
+// 		}
+// 	}
+// 	return
+// }
