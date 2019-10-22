3fdcc15b4f5a3324005e427857464520c6b97d12
Blacknon <blacknon@orebibou.com>
2019-10-23 07:58:16 +0900

    change comment

--- sftp/cmd_cat.go
+++ sftp/cmd_cat.go
@@ -7,14 +7,68 @@
 
 package sftp
 
-// TODO(blacknon): 複数ホスト接続時に、diffオプションがあるとうれしい？(もしくは、別途diffコマンドを開発か)
+import (
+	"fmt"
+
+	"github.com/blacknon/lssh/common"
+	"github.com/urfave/cli"
+)
 
 // cat is remote cat command
 func (r *RunSftp) cat(args []string) {
+	// create app
+	app := cli.NewApp()
+	// app.UseShortOptionHandling = true
 
+	// set help message
+	app.CustomAppHelpTemplate = helptext
+
+	app.Name = "cat"
+	app.Usage = "lsftp build-in command: cat [remote machine cat]"
+	app.ArgsUsage = "[PATH]"
+	app.HideHelp = true
+	app.HideVersion = true
+	app.EnableBashCompletion = true
+
+	app.Action = func(c *cli.Context) error {
+		argpath := c.Args().First()
+
+		//
+		fmt.Println(argpath)
+		return nil
+	}
+
+	// parse short options
+	args = common.ParseArgs(app.Flags, args)
+	app.Run(args)
 }
 
 // lcat is local cat command
 func (r *RunSftp) lcat(args []string) {
+	// create app
+	app := cli.NewApp()
+	// app.UseShortOptionHandling = true
+
+	// set help message
+	app.CustomAppHelpTemplate = helptext
+
+	app.Name = "lcat"
+	app.Usage = "lsftp build-in command: lcat [local machine cat]"
+	app.ArgsUsage = "[PATH]"
+	app.HideHelp = true
+	app.HideVersion = true
+	app.EnableBashCompletion = true
+
+	app.Action = func(c *cli.Context) error {
+		argpath := c.Args().First()
+
+		//
+		fmt.Println(argpath)
+		return nil
+	}
+
+	// parse short options
+	args = common.ParseArgs(app.Flags, args)
+	app.Run(args)
 
 }
