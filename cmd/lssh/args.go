3e05bf0e084da3d52755665ff3eda2e724665281
blacknon <blacknon@orebibou.com>
2022-01-08 14:54:24 +0900

    update v0.6.4

--- cmd/lssh/args.go
+++ cmd/lssh/args.go
@@ -65,7 +65,7 @@ USAGE:
 	app.Name = "lssh"
 	app.Usage = "TUI list select and parallel ssh client command."
 	app.Copyright = "blacknon(blacknon@orebibou.com)"
-	app.Version = "0.6.2"
+	app.Version = "0.6.4"
 
 	// TODO(blacknon): オプションの追加
 	//     -f       ... バックグラウンドでの接続(X11接続やport forwardingをバックグラウンドで実行する場合など)。
