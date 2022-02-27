a5e270bc4347311a93e7eccf1d0f5259b9d0a8c8
blacknon <blacknon@orebibou.com>
2022-02-27 19:22:16 +0900

    update. lsftpのdfコマンドでホストを指定可能へ.

--- sftp/cmd_df.go
+++ sftp/cmd_df.go
@@ -35,87 +35,110 @@ func (r *RunSftp) df(args []string) {
 	}
 	app.Name = "df"
 	app.Usage = "lsftp build-in command: df [remote machine df]"
-	app.ArgsUsage = "[PATH]"
+	app.ArgsUsage = "PATH..."
 	app.HideHelp = true
 	app.HideVersion = true
 	app.EnableBashCompletion = true
 
 	// action
 	app.Action = func(c *cli.Context) error {
-		argpath := c.Args().First()
+		argpathlist := c.Args()
 
 		// get remote stat data
-		stats := map[string]*sftp.StatVFS{}
-		for server, client := range r.Client {
-			// set ftp client
-			ftp := client.Connect
-
-			// set path
-			path := client.Pwd
-			if len(argpath) > 0 {
-				path = argpath
-			}
-
-			// get StatVFS
-			stat, err := ftp.StatVFS(path)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error: %s: %s\n", server, err)
-				continue
-			}
-			stats[server] = stat
-		}
+		stats := map[string]map[string]*sftp.StatVFS{}
 
 		// set tabwriter
 		tabw := new(tabwriter.Writer)
 		tabw.Init(os.Stdout, 0, 8, 4, ' ', tabwriter.AlignRight)
+
+		if len(argpathlist) == 0 {
+			argpathlist = append(argpathlist, "./")
+		}
+
+		targetmap := map[string]*TargetConnectMap{}
+		for _, p := range argpathlist {
+			targetmap = r.createTargetMap(targetmap, p)
+		}
+
+		for server, client := range targetmap {
+			// set map
+			stats[server] = map[string]*sftp.StatVFS{}
+
+			// set ftp client
+			ftp := client.Connect
+
+			// set path
+			pathlist := []string{}
+			if len(client.Path) > 0 {
+				pathlist = append(pathlist, client.Path...)
+			} else {
+				pathlist = append(pathlist, client.Pwd)
+			}
+
+			// get StatVFS
+			for _, path := range pathlist {
+				stat, err := ftp.StatVFS(path)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Error: %s: %s\n", server, err)
+					continue
+				}
+				stats[server][path] = stat
+			}
+
+		}
 
 		// print header
 		headerTotal := "TotalSize"
 		if c.Bool("i") {
 			headerTotal = "Inodes"
 		}
-		fmt.Fprintf(tabw, "%s\t%s\t%s\t%s\t%s\t\n", "Server", headerTotal, "Used", "(root)", "Capacity")
+		fmt.Fprintf(tabw, "%s\t%s\t%s\t%s\t%s\t%s\t\n", "Server", "Path", headerTotal, "Used", "(root)", "Capacity")
 
 		// print stat
-		for server, stat := range stats {
-			// set data in columns
-			var column1, column2, column3, column4, column5 string
-			switch {
-			case c.Bool("i"):
-				totals := stat.Files
-				frees := stat.Ffree
-				useds := totals - frees
+		for server, statlist := range stats {
+			for path, stat := range statlist {
+				// set data in columns
+				var column1, column2, column3, column4, column5, column6 string
+				switch {
+				case c.Bool("i"):
+					totals := stat.Files
+					frees := stat.Ffree
+					useds := totals - frees
 
-				column1 = server
-				column2 = strconv.FormatUint(totals, 10)
-				column3 = strconv.FormatUint(useds, 10)
-				column4 = strconv.FormatUint(frees, 10)
-				column5 = fmt.Sprintf("%0.2f", (float64(useds)/float64(totals))*100)
+					column1 = server
+					column2 = path
+					column3 = strconv.FormatUint(totals, 10)
+					column4 = strconv.FormatUint(useds, 10)
+					column5 = strconv.FormatUint(frees, 10)
+					column6 = fmt.Sprintf("%0.2f", (float64(useds)/float64(totals))*100)
 
-			case c.Bool("h"):
-				totals := stat.TotalSpace()
-				frees := stat.FreeSpace()
-				useds := stat.TotalSpace() - stat.FreeSpace()
+				case c.Bool("h"):
+					totals := stat.TotalSpace()
+					frees := stat.FreeSpace()
+					useds := stat.TotalSpace() - stat.FreeSpace()
 
-				column1 = server
-				column2 = humanize.IBytes(totals)
-				column3 = humanize.IBytes(useds)
-				column4 = humanize.IBytes(frees)
-				column5 = fmt.Sprintf("%0.2f", (float64(useds)/float64(totals))*100)
+					column1 = server
+					column2 = path
+					column3 = humanize.IBytes(totals)
+					column4 = humanize.IBytes(useds)
+					column5 = humanize.IBytes(frees)
+					column6 = fmt.Sprintf("%0.2f", (float64(useds)/float64(totals))*100)
 
-			default:
-				totals := stat.TotalSpace()
-				frees := stat.FreeSpace()
-				useds := stat.TotalSpace() - stat.FreeSpace()
+				default:
+					totals := stat.TotalSpace()
+					frees := stat.FreeSpace()
+					useds := stat.TotalSpace() - stat.FreeSpace()
 
-				column1 = server
-				column2 = strconv.FormatUint(totals/1024, 10)
-				column3 = strconv.FormatUint(useds/1024, 10)
-				column4 = strconv.FormatUint(frees/1024, 10)
-				column5 = fmt.Sprintf("%0.2f", (float64(useds)/float64(totals))*100)
+					column1 = server
+					column2 = path
+					column3 = strconv.FormatUint(totals/1024, 10)
+					column4 = strconv.FormatUint(useds/1024, 10)
+					column5 = strconv.FormatUint(frees/1024, 10)
+					column6 = fmt.Sprintf("%0.2f", (float64(useds)/float64(totals))*100)
+				}
+
+				fmt.Fprintf(tabw, "%s\t%s\t%s\t%s\t%s\t%s%%\t\n", column1, column2, column3, column4, column5, column6)
 			}
-
-			fmt.Fprintf(tabw, "%s\t%s\t%s\t%s\t%s%%\t\n", column1, column2, column3, column4, column5)
 		}
 
 		// write tabwriter
