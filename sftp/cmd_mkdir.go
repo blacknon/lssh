bb38032d677cd4c13588f9f77ee72887cf89ba67
blacknon <blacknon@orebibou.com>
2022-02-27 23:04:38 +0900

    update. lsftpでのpathだけのcomplete処理の追加前(多分別関数が必要)

--- sftp/cmd_mkdir.go
+++ sftp/cmd_mkdir.go
@@ -39,7 +39,7 @@ func (r *RunSftp) mkdir(args []string) {
 	// action
 	app.Action = func(c *cli.Context) error {
 		// TODO(blacknon): 複数のディレクトリ受付(v0.6.2以降)
-		if len(c.Args()) <= 1 {
+		if len(c.Args()) < 1 {
 			fmt.Println("Requires more one arguments")
 			fmt.Println("mkdir path...")
 			return nil
