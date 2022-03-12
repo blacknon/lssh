462dcd2559f5eeb3823adcc9ad2454695f59eeb1
blacknon <blacknon@orebibou.com>
2022-03-12 23:19:42 +0900

    update. github actions

--- list/list_windows.go
+++ list/list_windows.go
@@ -1,8 +1,8 @@
 // Copyright (c) 2021 Blacknon. All rights reserved.
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
-//go:build !windows && !plan9 && !nacl
-// +build !windows,!plan9,!nacl
+//go:build windows
+// +build windows
 
 package list
 
