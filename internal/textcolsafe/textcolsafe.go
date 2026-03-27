8d7bb9dfbb96e7484dfd6c728ef8c1e308fd7b79
Blacknon <blacknon@orebibou.com>
2026-03-28 02:02:43 +0900

    update and copyed lib.

--- internal/textcolsafe/textcolsafe.go
+++ internal/textcolsafe/textcolsafe.go
@@ -3,14 +3,14 @@
 package textcolsafe
 
 import (
-    "fmt"
-    "io"
-    "os"
-    "strconv"
-    "strings"
-    "unicode/utf8"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"unicode/utf8"
 
-    "github.com/acarl005/stripansi"
+	"github.com/acarl005/stripansi"
 )
 
 // PrintColumns prints items into columns safely without relying on external `stty`.
@@ -19,79 +19,79 @@ import (
 // - margin: spaces between columns
 // - padding: left padding to subtract from terminal width (e.g., prompt length)
 func PrintColumns(w io.Writer, strs []string, margin int, padding int) {
-    maxLength := 0
-    marginStr := strings.Repeat(" ", margin)
-    lengths := []int{}
-    for _, str := range strs {
-        colorless := stripansi.Strip(str)
-        length := utf8.RuneCountInString(colorless)
-        if length > maxLength {
-            maxLength = length
-        }
-        lengths = append(lengths, length)
-    }
+	maxLength := 0
+	marginStr := strings.Repeat(" ", margin)
+	lengths := []int{}
+	for _, str := range strs {
+		colorless := stripansi.Strip(str)
+		length := utf8.RuneCountInString(colorless)
+		if length > maxLength {
+			maxLength = length
+		}
+		lengths = append(lengths, length)
+	}
 
-    width := getTermWidth()
-    width = width - padding
+	width := getTermWidth()
+	width = width - padding
 
-    numCols, numRows := calculateTableSize(width, margin, maxLength, len(strs))
+	numCols, numRows := calculateTableSize(width, margin, maxLength, len(strs))
 
-    if numCols == 1 {
-        for _, str := range strs {
-            fmt.Fprintln(w, str)
-        }
-        return
-    }
+	if numCols == 1 {
+		for _, str := range strs {
+			fmt.Fprintln(w, str)
+		}
+		return
+	}
 
-    for i := 0; i < numCols*numRows; i++ {
-        x, y := rowIndexToTableCoords(i, numCols)
-        j := tableCoordsToColIndex(x, y, numRows)
+	for i := 0; i < numCols*numRows; i++ {
+		x, y := rowIndexToTableCoords(i, numCols)
+		j := tableCoordsToColIndex(x, y, numRows)
 
-        strLen := 0
-        str := ""
-        if j < len(lengths) {
-            strLen = lengths[j]
-            str = strs[j]
-        }
+		strLen := 0
+		str := ""
+		if j < len(lengths) {
+			strLen = lengths[j]
+			str = strs[j]
+		}
 
-        numSpacesRequired := maxLength - strLen
-        spaceStr := strings.Repeat(" ", numSpacesRequired)
+		numSpacesRequired := maxLength - strLen
+		spaceStr := strings.Repeat(" ", numSpacesRequired)
 
-        fmt.Fprintf(w, str)
+		fmt.Fprintf(w, str)
 
-        if x+1 == numCols {
-            fmt.Fprintf(w, "\n")
-        } else {
-            fmt.Fprintf(w, spaceStr)
-            fmt.Fprintf(w, marginStr)
-        }
-    }
+		if x+1 == numCols {
+			fmt.Fprintf(w, "\n")
+		} else {
+			fmt.Fprintf(w, spaceStr)
+			fmt.Fprintf(w, marginStr)
+		}
+	}
 }
 
 func getTermWidth() int {
-    if col := os.Getenv("COLUMNS"); col != "" {
-        if w, err := strconv.Atoi(col); err == nil {
-            return w
-        }
-    }
-    return 80
+	if col := os.Getenv("COLUMNS"); col != "" {
+		if w, err := strconv.Atoi(col); err == nil {
+			return w
+		}
+	}
+	return 80
 }
 
 func calculateTableSize(width, margin, maxLength, numCells int) (int, int) {
-    numCols := (width + margin) / (maxLength + margin)
-    if numCols == 0 {
-        numCols = 1
-    }
-    numRows := (numCells + numCols - 1) / numCols
-    return numCols, numRows
+	numCols := (width + margin) / (maxLength + margin)
+	if numCols == 0 {
+		numCols = 1
+	}
+	numRows := (numCells + numCols - 1) / numCols
+	return numCols, numRows
 }
 
 func rowIndexToTableCoords(i, numCols int) (int, int) {
-    x := i % numCols
-    y := i / numCols
-    return x, y
+	x := i % numCols
+	y := i / numCols
+	return x, y
 }
 
 func tableCoordsToColIndex(x, y, numRows int) int {
-    return y + numRows*x
+	return y + numRows*x
 }
