6e56b6ecf0e2b04744ba4f878b21ed29509047d9
Blacknon <blacknon@orebibou.com>
2026-04-08 22:53:37 +0900

    update. lscp and lsftp update.

--- internal/scp/path_resolution.go
+++ internal/scp/path_resolution.go
@@ -20,6 +20,16 @@ func shouldTreatLocalDestinationAsDir(destination string, forceDir bool) bool {
 	return err == nil && info.IsDir()
 }
 
+func copySourceBase(sourcePath string, sourceIsDir, preserveSourceName bool) string {
+	sourcePath = filepath.Clean(sourcePath)
+
+	if sourceIsDir && !preserveSourceName {
+		return sourcePath
+	}
+
+	return filepath.Dir(sourcePath)
+}
+
 func shouldTreatRemoteDestinationAsDir(destination string, targetExistsAsDir, sourceIsDir, multipleSources bool) bool {
 	return targetExistsAsDir || sourceIsDir || multipleSources || strings.HasSuffix(destination, "/")
 }
