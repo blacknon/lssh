adbc17191df2e937494795930352ef0b7a7b7e45
Blacknon <blacknon@orebibou.com>
2026-04-17 09:46:54 +0900

    update.

--- provider/secret/provider-secret-bitwarden/link_linux.go
+++ provider/secret/provider-secret-bitwarden/link_linux.go
@@ -3,6 +3,7 @@
 package main
 
 /*
-#cgo LDFLAGS: -lm
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/../../../vendor/github.com/bitwarden/sdk-go/internal/cinterface/lib/linux-x64 -Wl,--start-group -lbitwarden_c -lm -Wl,--end-group
+#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/../../../vendor/github.com/bitwarden/sdk-go/internal/cinterface/lib/linux-arm64 -Wl,--start-group -lbitwarden_c -lm -Wl,--end-group
 */
 import "C"
